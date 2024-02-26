a35b9c5f8dfe57798d4bcaa80f5a11e4a6817a22
agsantoro <adrian.santoro@gmail.com>
2024-02-26 16:00:24 -0300

    comentario

--- functions/graf_esc.R
+++ functions/graf_esc.R
@@ -1,7 +1,10 @@
 graf_esc = function(table, output) {
   charts <- lapply(1:nrow(table), function(i) {
     output[[paste0("graf",i)]]  = renderHighchart({
+      
+      # Gráfico que se muestra para cada indicador
       hchart(object = table[i,], type = "bar", hcaes(x = indicador, y = valor), color ='red')
+      
     })
   })
   
