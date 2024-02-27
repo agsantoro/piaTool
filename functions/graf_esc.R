980250b9141b2d8b67941df7c4cfa77b3377b5b4
velenp <velenpennini@gmail.com>
2024-02-27 06:58:54 -0300

    graficos

--- functions/graf_esc.R
+++ functions/graf_esc.R
@@ -3,7 +3,17 @@ graf_esc = function(table, output) {
     output[[paste0("graf",i)]]  = renderHighchart({
       
       # Gráfico que se muestra para cada indicador
-      hchart(object = table[i,], type = "bar", hcaes(x = indicador, y = valor), color ='red')
+      
+      
+      hchart(object = table[i,], type = "bar", hcaes(x = indicador, y = valor)) %>%
+        hc_colors(colors = c("#339BDF")) %>%
+        hc_plotOptions(series = list(pointWidth = 50, borderColor = 'black', borderWidth = 1)) %>%  # Agrega borde a la barra
+        hc_title(text = table[i,1]) %>%
+        hc_xAxis(title = list(text = ""), labels = list(enabled = FALSE)) %>% 
+        hc_yAxis(title = list(text = "Valor"), labels = list(enabled = TRUE), 
+                 plotLines = list(list(color = "black", width = 3, value = 0))) %>% 
+        hc_tooltip(pointFormat = 'Valor: {point.y:.1f}') %>% 
+        hc_add_theme(hc_theme_hcrt())  # Aplica el tema 'hcrt'
       
     })
   })
