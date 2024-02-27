2ba3be89358c95b0e9fde12b1bf60f08659c007e
velenp <velenpennini@gmail.com>
2024-02-27 13:26:04 -0300

    graf

--- functions/graf_esc.R
+++ functions/graf_esc.R
@@ -7,12 +7,19 @@ graf_esc = function(table, output) {
       
       hchart(object = table[i,], type = "bar", hcaes(x = indicador, y = valor)) %>%
         hc_colors(colors = c("#339BDF")) %>%
-        hc_plotOptions(series = list(pointWidth = 50, borderColor = 'black', borderWidth = 1)) %>%  # Agrega borde a la barra
+        hc_plotOptions(series = list(pointWidth = 50, borderColor = 'black',
+                                     borderWidth = 1,
+                                     groupPadding = -1,  # Reduce el padding entre grupos de barras
+                                     pointPadding = -4
+                                     )) %>%  
         hc_title(text = table[i,1]) %>%
-        hc_xAxis(title = list(text = ""), labels = list(enabled = FALSE)) %>% 
-        hc_yAxis(title = list(text = "Valor"), labels = list(enabled = TRUE), 
+        hc_xAxis(title = list(text = ""), labels = list(enabled = FALSE),
+                 startOnTick = FALSE, endOnTick = FALSE) %>% 
+        hc_yAxis(title = list(text = "Valor"), labels = list(enabled = TRUE),
                  plotLines = list(list(color = "black", width = 3, value = 0))) %>% 
         hc_tooltip(pointFormat = 'Valor: {point.y:.1f}') %>% 
+        hc_size(height= 220) %>% 
+        #hc_chart(margin = c(10, 10, 10, 3)) %>%  # Ajusta los márgenes del gráfico
         hc_add_theme(hc_theme_hcrt())  # Aplica el tema 'hcrt'
       
     })
